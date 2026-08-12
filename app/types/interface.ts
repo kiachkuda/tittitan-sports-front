92cedd3f46c60216db4130746413b2a99bf619c5
kiachkuda <wanjirusamkiarie@gmail.com>
2026-08-12 18:39:44 +0300

    working with create oder 2

--- app/types/interface.ts
+++ app/types/interface.ts
@@ -1,6 +1,6 @@
 
 export type Category = {category_id:number, name:string}
-export type Variant = {  variant_id:number, size: string; stock_quantity: number; sku: string };
+export type Variant = {  variant_id?:number, size: string; stock_quantity: number; sku: string };
 export type ImageType = {product_id: number; image_path: string, file:File, is_cover:boolean, id:number}
 
 export const SIZES = ["XS","S","M","L","XL","2xl"];
