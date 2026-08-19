e7015c49b4b1f9de33eb95c7599aef9c98189960
kiachkuda <wanjirusamkiarie@gmail.com>
2026-08-19 08:55:29 +0300

    payment status 1

--- app/components/checkout/CheckoutFlow.tsx
+++ app/components/checkout/CheckoutFlow.tsx
@@ -99,7 +99,7 @@ const initialState: CheckoutState = {
       console.log("order response: ",response)
 
       const paymentStatus =
-        response.data.payments.status;
+        response.data.payments[0].status;
 
       console.log(
         "Payment status:",
