15f73117e9895fc8df4542fee6ef0d8edecea090
kiachkuda <wanjirusamkiarie@gmail.com>
2026-08-24 16:33:44 +0300

    navigation bar

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -51,10 +51,10 @@ export default function Nav() {
          
            :
           <>
-          <Link className={`rounded-full px-4 py-2 text-sm font-medium transition-colors ${
-               "hover:bg-black hover:text-white"
-              }`} href={"/login"}>Login</Link>
-          <Link href={"/signup"}>signup</Link>
+          <Link className={`rounded-full px-4 py-2 text-sm font-medium transition-colors`}
+           href={"/login"}>Login</Link>
+          <Link  className={`rounded-full px-4 py-2 text-sm font-medium transition-colors`}
+            href={"/signup"}>signup</Link>
           </> }
         </nav>
 
